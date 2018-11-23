d871897616aebacb020cda4aa5d0926744eed23b
Mauricio Neira <m.neira10@uniandes.edu.co>
2018-11-23 07:46:32 -0500

    normalized bin size, normalized distribution

--- walk/graph.py
+++ walk/graph.py
@@ -4,8 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 data = np.loadtxt('serial.dat')
-plt.hist(data,label='serial',alpha = 0.7,bins=30)
+plt.hist(data,label='serial',alpha = 0.7,bins=np.linspace(0,2000,50),density=True)
 data = np.loadtxt('parallel.dat')
-plt.hist(data,label='parallel',alpha=0.7,bins=30)
+plt.hist(data,label='parallel',alpha=0.7,bins=np.linspace(0,2000,50),density=True)
 plt.legend()
 plt.savefig('compare.pdf')
